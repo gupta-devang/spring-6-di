2aa4eddc92c5cb5004b72f36cfa1eeacc1efa32d
Devang Gupta <devang.gupta67@gmail.com>
2024-12-18 17:25:05 +0530

    tested the context in test class

--- src/test/java/guru/springframework/spring6di/Spring6DiApplicationTests.java
+++ src/test/java/guru/springframework/spring6di/Spring6DiApplicationTests.java
@@ -1,10 +1,34 @@
 package guru.springframework.spring6di;
 
+import guru.springframework.spring6di.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class Spring6DiApplicationTests {
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Autowired
+    MyController controller;
+
+    @Test
+    void testAutoWiredOfController() {
+        assertNotNull(controller);
+        System.out.println((controller.sayHello()));
+    }
+
+    @Test
+    void testGetControllerFromCtx() {
+        MyController myController = applicationContext.getBean(MyController.class);
+        assertNotNull(myController);
+        System.out.println(myController.sayHello());
+    }
 
     @Test
     void contextLoads() {
